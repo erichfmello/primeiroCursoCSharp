f047c638b3451afccb80e3e22ae3f7c8002af7e2
Erich Mello <erich.mello@ad.wetoksoft.com.br>
2019-05-30 17:35:07 -0300

    Adicao 03

---
+++ projetos/ExercicioSecao6-02-01/ExercicioSecao6-02-01/Estudante.cs
@@ -0,0 +1,17 @@
+﻿namespace ExercicioSecao6_02_01 {
+    class Estudante {
+        public int NumeroQuarto { get; set; }
+        public string NomeEstudante { get; set; }
+        public string EmailEstudante { get; set; }
+
+        public Estudante (int numeroQuarto, string nomeEstudante, string emailEstudante) {
+            NumeroQuarto = numeroQuarto;
+            NomeEstudante = nomeEstudante;
+            EmailEstudante = emailEstudante;
+        }
+
+        public override string ToString() {
+            return NumeroQuarto + ": " + NomeEstudante + ", " + EmailEstudante;
+        }
+    }
+}
---
+++ projetos/ExercicioSecao6-02-01/ExercicioSecao6-02-01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExercicioSecao6_02_01 {
+    class Program {
+        static void Main(string[] args) {
+            Estudante[] estudantes = new Estudante[10];
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine("Aluguel #" + (i + 1));
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int j = int.Parse(Console.ReadLine());
+                estudantes[j] = new Estudante(j, nome, email);
+                Console.WriteLine("");
+            }
+
+            for (int i = 0; i < 10; i++) {
+                if (estudantes[i] != null) {
+                    Console.WriteLine(estudantes[i].ToString());
+                }
+            }
+        }
+    }
+}
