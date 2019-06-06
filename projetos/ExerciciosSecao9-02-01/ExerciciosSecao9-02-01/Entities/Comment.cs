2bc0e1fbcf43b85ca06c3a3dc60a3a88698e6e91
Erich Mello <erich.mello@ad.wetoksoft.com.br>
2019-06-06 17:50:57 -0300

    Adicao 05

---
+++ projetos/ExerciciosSecao9-02-01/ExerciciosSecao9-02-01/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciciosSecao9_02_01.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ projetos/ExerciciosSecao9-03-01/ExerciciosSecao9-03-01/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciciosSecao9_03_01.Entities
+{
+    class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
